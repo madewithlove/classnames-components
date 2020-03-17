53b553c9003e70fe2b16857d7c6091f6e6c4362b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-03-17 15:40:09 +0100

    Revert "Only pass down props if element is not a string"
    This reverts commit edd5467eea5f166f2c7ca67c9e0228ff06399798.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const isString = stringToCheck =>
-  typeof stringToCheck === 'string' || stringToCheck instanceof String;
-
 const isFunction = functionToCheck =>
   functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
 
@@ -15,7 +12,7 @@ const parseParams = (...params) => props =>
 const classnamesComponents = element => (...params) => props =>
   React.createElement(element, {
     className: parseParams(props.className, ...params)(props),
-    ...(isString(element) ? {} : props),
+    ...props,
   });
 
 export default classnamesComponents;
