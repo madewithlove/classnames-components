0118b035a89c33d573666920c24d3b2b26ead94a
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-03-17 14:55:42 +0100

    initial implementation

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classnames from 'classnames';
+
+const isFunction = functionToCheck =>
+  functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+
+const parseParams = (...params) => props =>
+  params
+    .map(param => classnames(isFunction(param) ? param(props) : param))
+    .join(' ');
+
+const classnamesComponents = Component => (...params) => props =>
+  React.createElement(element, {
+    className: parseParams(...params)(props),
+    ...props,
+  });
+
+export default classnamesComponents;
